bf65e0d3e50c16698a6337f4e7edf653dac6748a
Shobhit <shobhit.singh@worksdelight.org>
2022-06-17 17:49:43 +0530

    fixed some changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Cart from "./Pages/Cart";
-import Form from './Pages/Form';
 import Navbar from "./Components/Navbar";
 import {Provider} from 'react-redux'
 import store from './Store/store';
@@ -17,7 +16,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/cart" element={<Cart />}></Route>
-            <Route path="/form" element={<Form />}></Route>
           </Routes>
         </BrowserRouter>
       </Provider>
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartSlice';
-import formReducer from './formSlice';
 import productReducers from "./productsSlice"
 const store  = configureStore({
     reducer:{
         cart : cartReducer,
         product:productReducers,
-        form :formReducer
     }, 
 });
 
